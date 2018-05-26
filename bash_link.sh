fcf9d217bb5b9369ababbdb2446cf33efffca92e
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-05-26 04:58:12 +0000

    Update

--- bash_link.sh
+++ bash_link.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cat ~/dotfiles/bash/.bash_profile >> ~/.bash_profile
-ln -si ~/dotfiles/bash/.bashrc ~/.bashrc
+cat ~/dotfiles/bash/.bashrc >> ~/.bashrc
 ln -si ~/dotfiles/bash/.bash_aliases ~/.bash_aliases
 ln -si ~/dotfiles/bash/.bash_gnu_aliases ~/.bash_gnu_aliases
 ln -si ~/dotfiles/bash/.bash_bsd_aliases ~/.bash_bsd_aliases
