f6ead054188aa4903d04529a04d27616dbae2e87
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-06-17 10:40:58 +0000

    Add addisional if, symlink

--- bash_link.sh
+++ bash_link.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cat ~/dotfiles/bash/.bashrc >> ~/.bashrc
+if [ -f ~/.bashrc ]; then
+    cat ~/dotfiles/bash/.bashrc >> ~/.bashrc
+else
+    ln -si ~/dotfiles/bash/.bashrc ~/.bashrc
+fi
 ln -si ~/dotfiles/bash/.bash_aliases ~/.bash_aliases
 ln -si ~/dotfiles/bash/.bash_gnu_aliases ~/.bash_gnu_aliases
 ln -si ~/dotfiles/bash/.bash_bsd_aliases ~/.bash_bsd_aliases
